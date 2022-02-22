9939ceeb6fac395f55e7ce3cc7fe92a154552e75
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-02-22 18:04:38 +0700

    Add files

---
+++ PomeloTest/PomeloTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomeloTestApp.swift
+//  PomeloTest
+//
+//  Created by ERX-C02G47CEQ05N on 22/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct PomeloTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
